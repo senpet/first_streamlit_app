ede332c6bff45a9f80bad4d59d44ee486b4851d6
Nicolas <nicolaspetitmangin9@gmail.com>
2022-08-04 14:25:36 +0200

    change

--- OneDrive - BearingPoint GmbH/Documents/Bearing/D&A/Snowflake/streamlit/streamlit_app.py
+++ OneDrive - BearingPoint GmbH/Documents/Bearing/D&A/Snowflake/streamlit/streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My parents New Helathy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
